7ffcd5f35ef8eca56ea55c7af1e5562424b991d0
Dũng <ntdung3101@gmail.com>
2023-03-15 07:20:43 +0700

    Initial commit

---
+++ src/main/java/com/swd391/simpleauction/SimpleauctionApplication.java
@@ -0,0 +1,13 @@
+package com.swd391.simpleauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleauctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleauctionApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/swd391/simpleauction/SimpleauctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swd391.simpleauction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleauctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
