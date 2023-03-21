d291c5ebc07246c60eeb9823a0c6c69c9e30b50f
Dũng <ntdung3101@gmail.com>
2023-03-22 02:18:05 +0700

    mapping entity

--- src/main/java/com/swd391/simpleauction/entities/BiddingHistory.java
+++ src/main/java/com/swd391/simpleauction/entities/BiddingHistory.java
@@ -1,10 +1,14 @@
 package com.swd391.simpleauction.entities;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,10 +23,11 @@ public class BiddingHistory {
 	@Column(name="bidding_id", nullable = false, updatable = false)
 	private Integer biddingId;
 	
-	@Column(name="transaction_id")
-	private Integer transactionId;
-	
 	@Column(name="final_price")
 	private Float finalPrice;
 		
+	@OneToOne
+	@JsonBackReference
+	@JoinColumn(name = "transactionId")
+	private Transaction transaction;
 }
--- src/main/java/com/swd391/simpleauction/entities/Item.java
+++ src/main/java/com/swd391/simpleauction/entities/Item.java
@@ -1,12 +1,20 @@
 package com.swd391.simpleauction.entities;
 
 import java.sql.Date;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -37,5 +45,12 @@ public class Item {
 	@Column(name="published")
 	private Boolean published;	
 	
-	private Integer userId;
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	@JsonBackReference
+	private User user;
+	
+	@OneToMany(mappedBy = "item", fetch = FetchType.LAZY)
+	@JsonManagedReference
+	private Set<Transaction> transaction ;
 }
