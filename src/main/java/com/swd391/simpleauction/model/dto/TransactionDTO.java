67b909ffd115ba258f725290572b94c20ce9b941
Dũng <ntdung3101@gmail.com>
2023-03-22 08:50:31 +0700

    validate dto

--- src/main/java/com/swd391/simpleauction/model/dto/TransactionDTO.java
+++ src/main/java/com/swd391/simpleauction/model/dto/TransactionDTO.java
@@ -1,5 +1,6 @@
 package com.swd391.simpleauction.model.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,11 +11,15 @@ import lombok.NoArgsConstructor;
 public class TransactionDTO {
 	private Integer transactionId;
 	
+	@NotNull
 	private Float transactionPrice;
 	
+	@NotNull
 	private Integer itemId;
 	
+	@NotNull
 	private Integer userId;
 	
+	@NotNull
 	private Integer biddingId;
 }
